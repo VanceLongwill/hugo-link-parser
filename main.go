91a49ecb572a467794d49967e1780e3d7994679a
Vance Longwill <vancelongwill@gmail.com>
2019-02-07 11:55:38 +0300

    find the links

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
-	// "github.com/russross/blackfriday"
+	"github.com/russross/blackfriday/v2"
 )
 
 // (?|(?<txt>(?<url>(?:ht|f)tps?://\S+(?<=\PP)))|\(([^)]+)\)\[(\g<url>)])
+
+func Visitor(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+	// fmt.Println(node.Type)
+	switch node.Type {
+	case blackfriday.Link:
+		fmt.Println(string(node.LinkData.Destination), string(node.LinkData.Title))
+	}
+	return 0
+}
 
 func main() {
 	fmt.Println(
--- main_test.go
+++ main_test.go
@@ -1,28 +1,35 @@
 package main
 
 import (
-	// "io/ioutil"
-	"fmt"
+	"github.com/russross/blackfriday/v2"
+	"io/ioutil"
 	"testing"
 )
 
 func TestLinkParser(t *testing.T) {
-	// f, fileErr := ioutil.Read
-	// if err != nil {
-	// 	fmt.Print(err)
-	// }
-	var tests = []string{
-		// Empty document.
-		"",
-		" ",
-		"[regex101](https://regex101.com/r/mL3hA8/1)",
+	var tests = [][]byte{
+		[]byte(""),
+		[]byte(" "),
+		[]byte("- something\n\t-child\n\t-another child\n"),
+		[]byte("[regex101](https://regex101.com/r/mL3hA8/1)"),
 
-		"[hugo docs content management section](https://gohugo.io/content-management/formats/)",
+		[]byte("[hugo docs content management section](https://gohugo.io/content-management/formats/)"),
 	}
 
 	for i := 0; i < len(tests); i++ {
-		fmt.Println(tests[i])
-		// t.Error(tests[i])
+		md := blackfriday.New()
+		node := md.Parse(tests[i])
+		node.Walk(Visitor)
 	}
 
+}
+
+func TestWithReadme(t *testing.T) {
+	md := blackfriday.New()
+	f, err := ioutil.ReadFile("README.md")
+	if err != nil {
+		t.Error(err)
+	}
+	node := md.Parse(f)
+	node.Walk(Visitor)
 }
